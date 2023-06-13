5ad031a898531e4b017cf94eba3aa348df1351e8
AL_WARD <AL_WARD@MAJD>
2023-06-13 20:40:00 +0300

    First Test For Git ....

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -10,7 +10,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
